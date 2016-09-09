809e62d48e73bf4706e2189d6628df0c96d91d66
steflenz <steflenz@uni-mainz.de>
2016-09-09 09:38:38 +0200

    Weiterer Fehler bei Tests gefixt

--- test/runtests.jl
+++ test/runtests.jl
@@ -10,8 +10,8 @@ include("BMTest.jl")
 
 x = rand([0.0 0.0 0.0 1.0 1.0], 1000, 10);
 
-@test_approx_eq_eps(DBMTest.bgrbmexactloglikelihoodvsbaserate(x, 10), 0, 1e-10)
-@test_approx_eq_eps(DBMTest.rbmexactloglikelihoodvsbaserate(x, 10), 0, 1e-10)
+@test_approx_eq_eps(BMTest.bgrbmexactloglikelihoodvsbaserate(x, 10), 0, 1e-10)
+@test_approx_eq_eps(BMTest.rbmexactloglikelihoodvsbaserate(x, 10), 0, 1e-10)
 
 nvisible = 10
 nhidden = 10
@@ -19,4 +19,3 @@ bgrbm = BMs.BernoulliGaussianRBM(zeros(nvisible, nhidden), rand(nvisible), ones(
 
 @test_approx_eq(BMs.logpartitionfunction(bgrbm, 1.0),
       BMs.exactlogpartitionfunction(bgrbm))
-
