7beefd90ab8795108186edd881da18ec50c3cdb7
steflenz <steflenz@uni-mainz.de>
2016-10-17 14:42:04 +0200

    Code zum Trainieren von DBMs und RBMs in neue Dateien aufgeteilt

---
+++ src/dbmtraining.jl
@@ -0,0 +1,540 @@
+
+typealias DBMParam Array{BernoulliRBM,1}
+typealias Particles Array{Array{Float64,2},1}
+typealias Particle Array{Array{Float64,1},1}
+
+
+"""
+A MultivisionDBM consists of several visible layers (may have different
+input types) and binary hidden layers.
+Nodes of different visible layers are connected to non-overlapping parts
+of the first hidden layer.
+"""
+type MultivisionDBM
+
+   visrbms::Vector{AbstractRBM}
+   visrbmsvisranges::Array{UnitRange{Int}}
+   visrbmshidranges::Array{UnitRange{Int}}
+
+   hiddbm::DBMParam
+
+   function MultivisionDBM(visrbms, hiddbm)
+      # initialize ranges of hidden units for different RBMs
+      nvisrbms = length(visrbms)
+
+      # calculate visible ranges
+      visrbmsvisranges = Array{UnitRange{Int}}(nvisrbms)
+      offset = 0
+      for i = 1:nvisrbms
+         nvisibleofcurrentvisrbm = length(visrbms[i].a)
+         visrbmsvisranges[i] = offset + (1:nvisibleofcurrentvisrbm)
+         offset += nvisibleofcurrentvisrbm
+      end
+
+      # calculate hidden ranges
+      visrbmshidranges = Array{UnitRange{Int}}(nvisrbms)
+      offset = 0
+      for i = 1:nvisrbms
+         nhiddenofcurrentvisrbm = length(visrbms[i].b)
+         visrbmshidranges[i] = offset + (1:nhiddenofcurrentvisrbm)
+         offset += nhiddenofcurrentvisrbm
+      end
+
+      new(visrbms, visrbmsvisranges, visrbmshidranges, hiddbm)
+   end
+end
+
+typealias AbstractDBM Union{DBMParam, MultivisionDBM}
+
+function MultivisionDBM{T<:AbstractRBM}(visrbms::Vector{T})
+   MultivisionDBM(visrbms, DBMParam())
+end
+
+function MultivisionDBM(visrbm::AbstractRBM)
+   MultivisionDBM([visrbm], DBMParam())
+end
+
+
+"""
+    addlayer!(mvdbm, x)
+Adds a pretrained layer to the MultivisionDBM, given the dataset `x` as input
+for the visible layer.
+The variables/columns of `x` are divided among the visible RBMs.
+"""
+function addlayer!(mvdbm::MultivisionDBM, x::Matrix{Float64};
+      islast::Bool = false,
+      nhidden::Int = size(x,2),
+      epochs::Int = 10,
+      learningrate::Float64 = 0.005,
+      learningrates::Vector{Float64} = learningrate * ones(epochs),
+      pcd::Bool = true,
+      cdsteps::Int = 1,
+      monitoring::Function = ((rbm, epoch) -> nothing))
+
+   # propagate input x up to last hidden layer
+   hh = visiblestofirsthidden(mvdbm, x)
+   for i = 1:length(mvdbm.hiddbm)
+      hh = hprob(mvdbm.hiddbm[i], hh, 2.0) # intermediate layer, factor is 2.0
+   end
+
+   upfactor = downfactor = 2.0
+   if islast
+      upfactor = 1.0
+   end
+
+   rbm = fitrbm(hh, nhidden = nhidden, epochs = epochs,
+         rbmtype = BernoulliRBM,
+         learningrate = learningrate,
+         learningrates = learningrates,
+         pcd = pcd, cdsteps = 1,
+         upfactor = upfactor, downfactor = downfactor,
+         monitoring = monitoring)
+
+   push!(mvdbm.hiddbm, rbm)
+   mvdbm
+end
+
+function combinedbiases(dbm::DBMParam)
+   biases = Particle(length(dbm) + 1)
+   biases[1] = dbm[1].a
+   for i = 2:length(dbm)
+      biases[i] = dbm[i].a + dbm[i-1].b
+   end
+   biases[end] = dbm[end].b
+   biases
+end
+
+
+"
+Convenience Wrapper to pretrain an RBM-Stack and to jointly adjust the weights using fitbm
+"
+function fitdbm(x, nhiddens::Array{Int,1}, epochs=10, nparticles=100; learningrate::Float64 = 0.005)
+   pretraineddbm = BMs.stackrbms(x, nhiddens = nhiddens, epochs = epochs, predbm = true, learningrate=learningrate)
+   fitbm(x, pretraineddbm, epochs = epochs, nparticles = nparticles)
+end
+
+
+"""
+    fitbm(x, dbm; ...)
+Performs fitting of a Deep Boltzmann Machine. Expects a training data set `x`
+and a pre-trained Deep Boltzmann machine `dbm` as arguments.
+
+# Optional keyword arguments:
+* `epoch`: number of training epochs
+* `nparticles`: number of particles used for sampling, default 100
+* The learning rate for each epoch is given as vector `learningrates`.
+* `monitoring`: A function that is executed after each training epoch.
+   It takes a DBM and the epoch as arguments.
+"""
+function fitbm(x::Array{Float64,2}, dbm::AbstractDBM;
+      epochs::Int = 10,
+      nparticles::Int = 100,
+      learningrate::Float64 = 0.005,
+      learningrates::Array{Float64,1} = learningrate*11.0 ./ (10.0 + (1:epochs)),
+      monitoring::Function = ((dbm, epoch) -> nothing))
+
+   if length(learningrates) < epochs
+      error("Not enough learning rates for training epochs")
+   end
+
+   particles = initparticles(dbm, nparticles)
+
+   for epoch=1:epochs
+      traindbm!(dbm, x, particles, learningrates[epoch])
+
+      # monitoring the learning process at the end of epoch
+      monitoring(dbm, epoch)
+   end
+
+   dbm
+end
+
+
+# TODO document
+function gibbssample!(particles::Particles,
+      mvdbm::MultivisionDBM,
+      nsteps::Int = 5, beta::Float64 = 1.0)
+
+   nhiddenlayers = length(mvdbm.hiddbm) + 1
+
+   for step in 1:nsteps
+
+      # save state of first hidden layer
+      oldstate = copy(particles[2])
+
+      # input of first hidden layer from second hidden layer
+      hinputtop = particles[3] * mvdbm.hiddbm[1].weights'
+      broadcast!(+, hinputtop, hinputtop, mvdbm.hiddbm[1].a')
+
+      for i = eachindex(mvdbm.visrbms)
+         hiddenrange = mvdbm.visrbmshidranges[i]
+         visiblerange = mvdbm.visrbmsvisranges[i]
+
+         # sample first hidden from visible layers of visrbms
+         # and second hidden layer
+         input = hinputtop[:,hiddenrange] +
+               hiddeninput(mvdbm.visrbms[i], particles[1][:,visiblerange])
+         particles[2][:,hiddenrange] =
+               bernoulli(sigm(beta * input))
+
+         # sample visible from old first hidden
+         particles[1][:,visiblerange] =
+               samplevisible(mvdbm.visrbms[i], oldstate[:,hiddenrange], beta)
+      end
+
+      # sample other hidden layers
+      for i = 2:nhiddenlayers
+         input = oldstate * mvdbm.hiddbm[i-1].weights
+         broadcast!(+, input, input, mvdbm.hiddbm[i-1].b')
+         if i < nhiddenlayers
+            input += particles[i+2] * mvdbm.hiddbm[i].weights'
+            broadcast!(+, input, input, mvdbm.hiddbm[i].a')
+         end
+         oldstate = copy(particles[i+1])
+         particles[i+1] = bernoulli(sigm(beta * input))
+      end
+   end
+
+   particles
+end
+
+function gibbssample!(particles::Particles, dbm::DBMParam,
+      steps::Int = 5, beta::Float64 = 1.0)
+
+   for step in 1:steps
+      oldstate = copy(particles[1])
+      for i = 1:length(particles)
+         input = zeros(particles[i])
+         if i < length(particles)
+            input += particles[i+1] * dbm[i].weights'
+            broadcast!(+, input, input, dbm[i].a')
+         end
+         if i > 1
+            input += oldstate * dbm[i-1].weights
+            broadcast!(+, input, input, dbm[i-1].b')
+         end
+         oldstate = copy(particles[i])
+         particles[i] = bernoulli(sigm(beta * input))
+      end
+   end
+
+   particles
+end
+
+
+"""
+    initparticles(dbm, nparticles)
+Creates particles for Gibbs sampling in a DBM and initializes them with random
+Bernoulli distributed (p=0.5) values.
+Returns an array containing in the i'th entry a matrix of size
+(`nparticles`, number of nodes in layer i) such that
+the particles are contained in the rows of these matrices.
+"""
+function initparticles(dbm::DBMParam, nparticles::Int)
+   nlayers = length(dbm) + 1
+   particles = Particles(nlayers)
+   particles[1] = rand([0.0 1.0], nparticles, length(dbm[1].a))
+   for i in 2:nlayers
+      particles[i] = rand([0.0 1.0], nparticles, length(dbm[i-1].b))
+   end
+   particles
+end
+
+function initparticles(mvdbm::MultivisionDBM, nparticles::Int)
+   nhidrbms = length(mvdbm.hiddbm)
+   if nhidrbms == 0
+      error("Add layers to MultivisionDBM to be able to call `initparticles`")
+   end
+
+   particles = Particles(nhidrbms + 2)
+   particles[1] = Matrix{Float64}(nparticles, mvdbm.visrbmsvisranges[end][end])
+   for i = 1:length(mvdbm.visrbms)
+      visiblerange = mvdbm.visrbmsvisranges[i]
+      # TODO treat Gaussian visible differently
+      particles[1][:,visiblerange] = rand([0.0 1.0], nparticles, length(mvdbm.visrbms[i].a))
+   end
+   particles[2:end] = initparticles(mvdbm.hiddbm, nparticles)
+   particles
+end
+
+
+"""
+    meanfield(dbm, x)
+    meanfield(dbm, x, eps)
+Computes the mean-field approximation for the data set `x` and
+returns a matrix of particles for the DBM.
+The number of particles is equal to the number of samples in `x`.
+`eps` is the convergence criterion for the fix-point iteration, default 0.001.
+"""
+function meanfield(dbm::DBMParam, x::Array{Float64,2}, eps::Float64 = 0.001)
+
+   nlayers = length(dbm) + 1
+   mu = Particles(nlayers)
+
+   # Initialization with single bottom-up pass using twice the weights for all
+   # but the topmost layer (see [Salakhutdinov+Hinton, 2012], p. 1985)
+   mu[1] = x
+   for i=2:(nlayers-1)
+      mu[i] = hprob(dbm[i-1], mu[i-1], 2.0)
+   end
+   mu[nlayers] = hprob(dbm[nlayers-1], mu[nlayers-1])
+
+   # mean-field updates until convergence criterion is met
+   delta = 1.0
+   while delta > eps
+      delta = 0.0
+
+      for i=2:(nlayers-1)
+         newmu = mu[i+1]*dbm[i].weights' + mu[i-1]*dbm[i-1].weights
+         broadcast!(+, newmu, newmu, (dbm[i-1].b + dbm[i].a)')
+         newmu = sigm(newmu)
+         newdelta = maximum(abs(mu[i] - newmu))
+         if newdelta > delta
+            delta = newdelta
+         end
+         mu[i] = newmu
+      end
+
+      # last layer
+      newmu = hprob(dbm[nlayers-1], mu[nlayers-1])
+      newdelta = maximum(abs(mu[nlayers] - newmu))
+      if newdelta > delta
+         delta = newdelta
+      end
+      mu[nlayers] = newmu
+   end
+
+   mu
+end
+
+function meanfield(mvdbm::MultivisionDBM, x::Array{Float64}, eps::Float64 = 0.001)
+
+   nlayers = length(mvdbm.hiddbm) + 2
+   nsamples = size(x,1)
+   nfirsthidden = length(mvdbm.hiddbm[1].a)
+   mu = Particles(nlayers)
+
+   # Initialization with single bottom-up pass
+   mu[1] = x
+   mu[2] = visiblestofirsthidden(mvdbm, x)
+   for i = 3:(nlayers-1) # intermediate hidden layers after second
+      mu[i] = hprob(mvdbm.hiddbm[i-2], mu[i-1], 2.0)
+   end
+   mu[nlayers] = hprob(mvdbm.hiddbm[nlayers-2], mu[nlayers-1])
+
+   # mean-field updates until convergence criterion is met
+   delta = Inf
+   while delta > eps
+      delta = 0.0
+
+      # input of first hidden layer from second hidden layer
+      hinputtop = mu[3] * mvdbm.hiddbm[1].weights'
+      newmu = Matrix{Float64}(nsamples, nfirsthidden)
+      for i = eachindex(mvdbm.visrbms)
+         hiddenrange = mvdbm.visrbmshidranges[i]
+         visiblerange = mvdbm.visrbmsvisranges[i]
+
+         newmu[:,hiddenrange] = sigm(hinputtop[:,hiddenrange] +
+               hiddeninput(mvdbm.visrbms[i], mu[1][:,visiblerange]))
+      end
+      delta = max(delta, maximum(abs(mu[2] - newmu)))
+      mu[2] = newmu
+
+      for i = 3:nlayers
+         input = mu[i-1] * mvdbm.hiddbm[i-2].weights
+         broadcast!(+, input, input, mvdbm.hiddbm[i-2].b')
+         if i < nlayers
+            input += mu[i+1] * mvdbm.hiddbm[i-1].weights'
+            broadcast!(+, input, input, mvdbm.hiddbm[i-1].a')
+         end
+         newmu = sigm(input)
+         delta = max(delta, maximum(abs(mu[i] - newmu)))
+         mu[i] = newmu
+      end
+   end
+
+   mu
+end
+
+
+# TODO document parameters
+"""
+Performs greedy layerwise training for Deep Belief Networks or greedy layerwise
+pretraining for Deep Boltzmann Machines.
+"""
+function stackrbms(x::Array{Float64,2};
+      nhiddens::Array{Int,1} = round(Int64, size(x,2) * ones(3)), # TODO besserer default-Wert für Anzahl hidden units
+      epochs::Int = 10,
+      predbm::Bool = false,
+      samplehidden::Bool = true,
+      learningrate::Float64 = 0.005,
+      layerwisemonitoring::Function = (rbmindex, rbm, epoch) -> nothing)
+
+   nrbms = length(nhiddens)
+   dbmn = Array{BernoulliRBM,1}(nrbms)
+
+   upfactor = downfactor = 1.0
+   if predbm
+      upfactor = 2.0
+   end
+
+   dbmn[1] = BMs.fitrbm(x, nhidden = nhiddens[1], epochs = epochs,
+         upfactor = upfactor, downfactor = downfactor, pcd = true,
+         learningrate = learningrate,
+         monitoring = (rbm, epoch) -> layerwisemonitoring(1, rbm, epoch))
+
+   hiddenval = x
+   for i=2:nrbms
+      hiddenval = BMs.hprob(dbmn[i-1], hiddenval, upfactor)
+      if samplehidden
+         hidden = bernoulli(hiddenval)
+      end
+      if predbm
+         upfactor = downfactor = 2.0
+         if i == nrbms
+            upfactor = 1.0
+         end
+      else
+         upfactor = downfactor = 1.0
+      end
+      dbmn[i] = BMs.fitrbm(hiddenval, nhidden = nhiddens[i], epochs = epochs,
+            upfactor = upfactor, downfactor = downfactor, pcd = true,
+            learningrate = learningrate,
+            monitoring = (rbm, epoch) -> layerwisemonitoring(i, rbm, epoch))
+   end
+
+   dbmn
+end
+
+
+"""
+    traindbm!(dbm, x, particles, learningrate)
+Trains the given `dbm` for one epoch.
+"""
+function traindbm!(dbm::DBMParam, x::Array{Float64,2}, particles::Particles,
+      learningrate::Float64)
+
+   gibbssample!(particles, dbm)
+   mu = meanfield(dbm, x)
+
+   for i = eachindex(dbm)
+      updatedbmpart!(dbm[i], learningrate,
+            particles[i], particles[i+1], mu[i], mu[i+1])
+   end
+
+   dbm
+end
+
+function traindbm!(mvdbm::MultivisionDBM, x::Array{Float64,2},
+      particles::Particles, learningrate::Float64)
+
+   gibbssample!(particles, mvdbm)
+   mu = meanfield(mvdbm, x)
+
+   # update parameters of each visible RBM
+   for i = eachindex(mvdbm.visrbms)
+      visiblerange = mvdbm.visrbmsvisranges[i]
+      hiddenrange = mvdbm.visrbmshidranges[i]
+
+      updatedbmpart!(mvdbm.visrbms[i], learningrate,
+            particles[1][:,visiblerange], particles[2][:,hiddenrange],
+            mu[1][:,visiblerange], mu[2][:,hiddenrange])
+   end
+
+   # update parameters of each RBM in hiddbm
+   for i = eachindex(mvdbm.hiddbm)
+      updatedbmpart!(mvdbm.hiddbm[i], learningrate,
+            particles[i+1], particles[i+2], mu[i+1], mu[i+2])
+   end
+
+   mvdbm
+end
+
+
+function updatedbmpart!(dbmpart::BernoulliRBM,
+      learningrate::Float64,
+      vgibbs::Matrix{Float64},
+      hgibbs::Matrix{Float64},
+      vmeanfield::Matrix{Float64},
+      hmeanfield::Matrix{Float64})
+
+   updatedbmpartcore!(dbmpart, learningrate,
+         vgibbs, hgibbs, vmeanfield, hmeanfield)
+end
+
+function updatedbmpart!(dbmpart::Binomial2BernoulliRBM,
+      learningrate::Float64,
+      vgibbs::Matrix{Float64},
+      hgibbs::Matrix{Float64},
+      vmeanfield::Matrix{Float64},
+      hmeanfield::Matrix{Float64})
+
+   vmeanfield /= 2
+   vgibbs /= 2
+
+   updatedbmpartcore!(dbmpart, learningrate,
+         vgibbs, hgibbs, vmeanfield, hmeanfield)
+end
+
+function updatedbmpart!(dbmpart::GaussianBernoulliRBM,
+      learningrate::Float64,
+      vgibbs::Matrix{Float64},
+      hgibbs::Matrix{Float64},
+      vmeanfield::Matrix{Float64},
+      hmeanfield::Matrix{Float64})
+
+   # For respecting standard deviation in update rule
+   # see [Srivastava+Salakhutdinov, 2014], p. 2962
+   vmeanfield = broadcast(./, vmeanfield, dbmpart.sd')
+   vgibbs = broadcast(./, vgibbs, dbmpart.sd')
+
+   updatedbmpartcore!(dbmpart, learningrate,
+         vgibbs, hgibbs, vmeanfield, hmeanfield)
+end
+
+function updatedbmpartcore!(dbmpart::AbstractRBM,
+      learningrate::Float64,
+      vgibbs::Matrix{Float64},
+      hgibbs::Matrix{Float64},
+      vmeanfield::Matrix{Float64},
+      hmeanfield::Matrix{Float64})
+
+   nsamples = size(vmeanfield, 1)
+   nparticles = size(vgibbs, 1)
+
+   leftw = vmeanfield' * hmeanfield / nsamples
+   lefta = mean(vmeanfield, 1)[:]
+   leftb = mean(hmeanfield, 1)[:]
+
+   rightw = vgibbs' * hgibbs / nparticles
+   righta = mean(vgibbs, 1)[:]
+   rightb = mean(hgibbs, 1)[:]
+
+   dbmpart.weights += learningrate*(leftw - rightw)
+   dbmpart.a += learningrate*(lefta - righta)
+   dbmpart.b += learningrate*(leftb - rightb)
+   nothing
+end
+
+
+"""
+    visiblestofirsthidden(mvdbm, x)
+Returns the activations induced by the forward pass of the dataset `x`
+as inputs for the visible layer.
+The variables/columns of `x` are divided among the visible RBMs.
+"""
+function visiblestofirsthidden(mvdbm::MultivisionDBM, x::Matrix{Float64})
+   nsamples = size(x, 1)
+   nvisrbms = length(mvdbm.visrbms)
+
+   probs = Vector{Matrix{Float64}}(nvisrbms)
+
+   for i = 1:nvisrbms
+      visiblerange = mvdbm.visrbmsvisranges[i]
+      input = hiddeninput(mvdbm.visrbms[i], x[:, visiblerange])
+      probs[i] = bernoulli(sigm(2.0 * input))
+   end
+
+   hcat(probs...)
+end
+
---
+++ src/rbmtraining.jl
@@ -0,0 +1,269 @@
+abstract AbstractRBM
+
+type BernoulliRBM <: AbstractRBM
+   weights::Array{Float64,2}
+   a::Array{Float64,1}
+   b::Array{Float64,1}
+end
+
+
+"""
+Parameters of a RBM with real valued, Gaussian distributed visible nodes and
+binary, Bernoulli distributed hidden nodes.
+"""
+type GaussianBernoulliRBM <: AbstractRBM
+   weights::Array{Float64,2}
+   a::Array{Float64,1}
+   b::Array{Float64,1}
+   sd::Array{Float64,1}
+end
+
+
+type BernoulliGaussianRBM <: AbstractRBM
+   weights::Array{Float64,2}
+   a::Array{Float64,1}
+   b::Array{Float64,1}
+end
+
+
+"""
+Contains the parameters of an RBM with 0/1/2-valued, Binomial (n=2) distributed
+visible nodes, and binary, Bernoulli distributed hidden nodes.
+This model is equivalent to a BernoulliRBM in which every two visible nodes are
+connected with the same weights to one hidden node.
+The states (0,0) / (1,0) / (0,1) / (1,1) of the visible nodes connected with
+with the same weights translate as states 0 / 1 / 1 / 2 in the
+Binomial2BernoulliRBM.
+"""
+type Binomial2BernoulliRBM <: AbstractRBM
+   weights::Matrix{Float64}
+   a::Vector{Float64}
+   b::Vector{Float64}
+end
+
+
+"""
+    fitrbm(x; ...)
+Fits an RBM model to the data set `x`, using Stochastic Gradient Descent (SGD)
+with Contrastive Divergence (CD), and returns it.
+
+# Optional keyword arguments (ordered by importance):
+* `rbmtype`: the type of the desired RBM. This must be a subtype of AbstractRBM
+   and defaults to `BernoulliRBM`.
+* `nhidden`: number of hidden units for returned RBM
+* `epoch`: number of training epochs
+* `learningrate`/`learningrates`: The learning rate for the weights and biases
+   can be specified as single value, used throughout all epochs, or as a vector
+   of `learningrates` that contains a value for each epoch.
+* `pcd`: indicating whether Persistent Contrastive Divergence (PCD) is to
+   be used (true, default) or simple CD that initializes the Gibbs Chain with
+   the training sample (false)
+* `cdsteps`: number of Gibbs sampling steps in CD/PCD, defaults to 1
+* `monitoring`: a function that is executed after each training epoch.
+   It takes an RBM and the epoch as arguments.
+* `upfactor`, `downfactor`: If this function is used for pretraining a part of
+   a DBM, it is necessary to multiply the weights of the RBM with factors.
+* `sdlearningrate`/`sdlearningrates`: learning rate(s) for the
+   standard deviation if training a `GaussianBernoulliRBM`. Ignored for other
+   types of RBMs. It usually must be much smaller than the learning rates for
+   the weights. By default, it is 0.0 and the standard deviation is not learned.
+"""
+function fitrbm(x::Matrix{Float64};
+      nhidden::Int = size(x,2),
+      epochs::Int = 10,
+      upfactor::Float64 = 1.0,
+      downfactor::Float64 = 1.0,
+      learningrate::Float64 = 0.005,
+      learningrates::Vector{Float64} = learningrate * ones(epochs),
+      pcd::Bool = true,
+      cdsteps::Int = 1,
+      rbmtype::DataType = BernoulliRBM,
+      monitoring::Function = ((rbm, epoch) -> nothing),
+
+      # these arguments are only relevant for GaussianBernoulliRBMs:
+      sdlearningrate::Float64 = 0.0,
+      sdlearningrates::Vector{Float64} = sdlearningrate * ones(epochs),
+      sdinitfactor::Float64 = 0.0)
+
+   rbm = initrbm(x, nhidden, rbmtype)
+   if sdinitfactor > 0 && rbmtype == GaussianBernoulliRBM
+      rbm.sd *= sdinitfactor
+   end
+
+   if length(learningrates) < epochs ||
+         (rbmtype == GaussianBernoulliRBM && length(sdlearningrates) < epochs)
+      error("Not enough learning rates for training epochs")
+   end
+
+   if pcd
+      chainstate = rand(nhidden)
+   else
+      chainstate = Array{Float64,1}()
+   end
+
+   for epoch = 1:epochs
+
+      # Train RBM on data set
+      trainrbm!(rbm, x, cdsteps = cdsteps, chainstate = chainstate,
+            upfactor = upfactor, downfactor = downfactor,
+            learningrate = learningrates[epoch],
+            sdlearningrate = sdlearningrates[epoch])
+
+      # Evaluation of learning after each training epoch
+      monitoring(rbm, epoch)
+   end
+
+   rbm
+end
+
+
+"""
+    initrbm(x, nhidden)
+    initrbm(x, nhidden, rbmtype)
+Creates a RBM with `nhidden` hidden units and initalizes its weights for
+training on dataset `x`.
+`rbmtype` can be a subtype of `AbstractRBM`, default is `BernoulliRBM`.
+"""
+function initrbm(x::Array{Float64,2}, nhidden::Int,
+      rbmtype::DataType = BernoulliRBM)
+
+   nsamples, nvisible = size(x)
+   weights = randn(nvisible, nhidden)/sqrt(nvisible)
+   hidbias = zeros(nhidden)
+
+   if rbmtype == BernoulliRBM
+      visbias = initvisiblebias(x)
+      return BernoulliRBM(weights, visbias, hidbias)
+
+   elseif rbmtype == GaussianBernoulliRBM
+      visbias = vec(mean(x, 1))
+      sd = vec(std(x, 1))
+      return GaussianBernoulliRBM(weights, visbias, hidbias, sd)
+
+   elseif rbmtype == BernoulliGaussianRBM
+      visbias = initvisiblebias(x)
+      return BernoulliGaussianRBM(weights, visbias, hidbias)
+
+   elseif rbmtype == Binomial2BernoulliRBM
+      visbias = initvisiblebias(x/2)
+      return Binomial2BernoulliRBM(weights, visbias, hidbias)
+
+   else
+      error(string("Datatype for RBM is unsupported: ", rbmtype))
+   end
+end
+
+
+"""
+    initvisiblebias(x)
+Returns sensible initial values for the visible bias for training an RBM
+on the data set `x`.
+"""
+function initvisiblebias(x::Array{Float64,2})
+   nvisible = size(x,2)
+   initbias = zeros(nvisible)
+   for j=1:nvisible
+      empprob = mean(x[:,j])
+      if empprob > 0
+         initbias[j] = log(empprob/(1-empprob))
+      end
+   end
+   initbias
+end
+
+
+function sdupdateterm(gbrbm::GaussianBernoulliRBM, v::Array{Float64,1}, h::Array{Float64,1})
+   (v - gbrbm.a).^2 ./ (gbrbm.sd .^3) - (v ./ (gbrbm.sd .^ 2)) .* (gbrbm.weights * h)
+end
+
+
+"""
+    trainrbm!(rbm, x)
+Trains the given `rbm` for one epoch. (See also function `fitrbm`.)
+
+# Optional keyword arguments:
+* `learningrate`, `cdsteps`, `sdlearningrate`, `upfactor`, `downfactor`:
+   See documentation of function `fitrbm`.
+* `chainstate`: a vector for holding the state of the RBM's hidden nodes. If
+   it is specified, PCD is used.
+"""
+function trainrbm!(rbm::AbstractRBM, x::Array{Float64,2};
+      chainstate::Array{Float64,1} = Array{Float64,1}(),
+      upfactor::Float64 = 1.0,
+      downfactor::Float64 = 1.0,
+      learningrate::Float64 = 0.005,
+      cdsteps::Int = 1,
+      sdlearningrate::Float64 = 0.0)
+
+   nsamples = size(x,1)
+
+   # perform PCD if a chain state is provided as parameter
+   pcd = !isempty(chainstate)
+
+   for j = 1:nsamples
+      v = vec(x[j,:])
+
+      h = samplehidden(rbm, v, upfactor)
+
+      # In case of CD, start Gibbs chain with the hidden state induced by the
+      # states of the visible units. In case of PCD, start Gibbs chain with
+      # previous state of the Gibbs chain.
+      hmodel = pcd ? chainstate : h
+
+      for step = 2:cdsteps
+         vmodel = samplevisible(rbm, hmodel, downfactor)
+         hmodel = samplehidden(rbm, vmodel, upfactor)
+      end
+
+      # Do not sample in last step to avoid unnecessary sampling noise
+      vmodel = vprob(rbm, hmodel, downfactor)
+      hmodel = hprob(rbm, vmodel, upfactor)
+
+      if pcd
+         # Preserve state of chain in a way that changes are visible to the caller.
+         copy!(chainstate, hmodel)
+         bernoulli!(chainstate) # TODO change to support other types of hidden nodes
+      end
+
+      updateparameters!(rbm, v, vmodel, h, hmodel, learningrate, sdlearningrate)
+   end
+
+   rbm
+end
+
+function updateparameters!(rbm::AbstractRBM,
+      v::Vector{Float64}, vmodel::Vector{Float64},
+      h::Vector{Float64}, hmodel::Vector{Float64},
+      learningrate::Float64,
+      sdlearningrate::Float64)
+
+   deltaw = v*h' - vmodel*hmodel'
+   rbm.weights += deltaw * learningrate
+   rbm.a += (v - vmodel) * learningrate
+   rbm.b += (h - hmodel) * learningrate
+   nothing
+end
+
+# See bottom of page 15 in [Krizhevsky, 2009].
+function updateparameters!(gbrbm::GaussianBernoulliRBM,
+      v::Vector{Float64}, vmodel::Vector{Float64},
+      h::Vector{Float64}, hmodel::Vector{Float64},
+      learningrate::Float64,
+      sdlearningrate::Float64)
+
+   sd = gbrbm.sd
+
+   if sdlearningrate > 0
+      gbrbm.sd -= (sdupdateterm(gbrbm, v, h) -
+            sdupdateterm(gbrbm, vmodel, hmodel)) * sdlearningrate
+   end
+
+   v = v ./ sd
+   vmodel = vmodel ./ sd
+
+   deltaw = v*h' - vmodel*hmodel'
+   gbrbm.weights += deltaw * learningrate
+   gbrbm.a += (v - vmodel) * learningrate
+   gbrbm.b += (h - hmodel) * learningrate
+   nothing
+end
