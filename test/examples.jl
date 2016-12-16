f3b5b4e852b66d6da27c043ab2725986aaa06111
Stefan Lenz <steflenz@uni-mainz.de>
2016-12-16 17:25:18 +0100

    Iris-Datensatz als Beispiel für GaussianBernoulliRBM + seeds

--- test/examples.jl
+++ test/examples.jl
@@ -7,15 +7,17 @@ using BoltzmannMachines
 # Generate data and split it in training and test data set
 nsamples = 100;
 nvariables = 36;
+srand(1);
 x = barsandstripes(nsamples, nvariables);
 x, xtest = splitdata(x, 0.3);
 datadict = DataDict("Training data" => x, "Test data" => xtest);
 
+
 # Fit a BernoulliRBM and collect monitoring data during training.
 # For small models, the model's loglikelihood can be computed exactly.
 monitor = Monitor();
-rbm = fitrbm(x; nhidden = 7,
-      epochs = 50, learningrate = 0.01,
+rbm = fitrbm(x; nhidden = 12,
+      epochs = 80, learningrate = 0.007,
       monitoring = (rbm, epoch) -> begin
             monitorexactloglikelihood!(monitor, rbm, epoch, datadict)
             monitorreconstructionerror!(monitor, rbm, epoch, datadict)
@@ -26,7 +28,7 @@ BMPlots.plotevaluation(monitor, monitorexactloglikelihood)
 BMPlots.plotevaluation(monitor, monitorreconstructionerror)
 
 
-# DBM-Fitting: Pretraining by adding layer by layer.
+# DBM-Fitting approach 1 - Pretraining, adding layer by layer.
 dbm = BasicDBM();
 
 # With this approach it is possible to also monitor the layerwise pretraining,
@@ -38,10 +40,9 @@ addlayer!(dbm, x; nhidden = 6, epochs = 20,
             monitorreconstructionerror!(monitor1, rbm, epoch, datadict));
 BMPlots.plotevaluation(monitor1, monitorreconstructionerror)
 
-addlayer!(dbm, x; nhidden = 5);
 addlayer!(dbm, x; nhidden = 2, islast = true);
 
-# DBM-Fitting: Fine-Tuning
+# DBM-Fitting approach 1 - Fine-Tuning
 monitor = Monitor();
 traindbm!(dbm, x; epochs = 10, learningrate = 0.05,
       monitoring = (dbm, epoch) ->
@@ -49,7 +50,7 @@ traindbm!(dbm, x; epochs = 10, learningrate = 0.05,
 
 BMPlots.plotevaluation(monitor, monitorexactloglikelihood)
 
-# DBM-Fitting: Pretraining and Fine-Tuning combined in one function
+# DBM-Fitting approach 2: Pretraining and Fine-Tuning combined in one function
 dbm = fitdbm(x, nhiddens = [6;5;2], epochs = 20, epochspretraining = 20,
       learningratepretraining = 0.05, learningrate = 0.05);
 
@@ -68,8 +69,10 @@ BMPlots.plotevaluation(monitor, monitorloglikelihood; sdrange = 3.0)
 
 
 # In the DBM, the estimation of the log likelihood is much slower than in the
-# RBM. The lower bound of the log probability is faster to calculate:
-# (But on the other hand, it might be too conservative.)
+# RBM because it needs a AIS run for each sample.
+# The lower bound of the log probability is faster to estimate:
+# (But on the other hand, in some cases it might be too conservative to be 
+# useful as it is only a lower bound.)
 
 monitor = Monitor();
 dbm = stackrbms(x; nhiddens = [36;10;5], predbm = true, learningrate = 0.05)
@@ -86,9 +89,20 @@ BMPlots.plotevaluation(monitor, monitorlogproblowerbound; sdrange = 3.0)
 # ==============================================================================
 # Real valued data: GaussianBernoulliRBM
 # ------------------------------------------------------------------------------
-# Generate real-valued data and train GaussianBernoulliRBM
-# For a small number of variables, the likelihood of the model can be computed
-# fast enough.
+
+# Use iris dataset as example data to train a GaussianBernoulliRBM
+using RDatasets
+x = convert(Matrix{Float64}, dataset("datasets", "iris")[1:4]);
+srand(12);
+x, xtest = splitdata(x, 0.3);
+datadict = DataDict("Training data" => x, "Test data" => xtest);
+monitor = Monitor();
+rbm = fitrbm(x, rbmtype = GaussianBernoulliRBM,
+      nhidden = 3, epochs = 70, learningrate = 0.001,
+      monitoring = (rbm, epoch) ->
+            monitorexactloglikelihood!(monitor, rbm, epoch, datadict));
+
+BMPlots.plotevaluation(monitor, monitorexactloglikelihood)
 
 
 # ==============================================================================
@@ -97,7 +111,8 @@ BMPlots.plotevaluation(monitor, monitorlogproblowerbound; sdrange = 3.0)
 
 # Generate data and split it in training and test data set
 nsamples = 100;
-nvariables = 36;
+nvariables = 25;
+srand(12);
 x = barsandstripes(nsamples, nvariables) + barsandstripes(nsamples, nvariables);
 x, xtest = splitdata(x, 0.3);
 datadict = DataDict("Training data" => x, "Test data" => xtest);
@@ -105,10 +120,9 @@ datadict = DataDict("Training data" => x, "Test data" => xtest);
 # Fit Binomial2BernoulliRBM.
 monitor = Monitor();
 rbm = fitrbm(x, rbmtype = Binomial2BernoulliRBM,
-      nhidden = 4, nhidden = div(nvariables,2),
-      epochs = 50, learningrate = 0.002,
+      nhidden = 10, epochs = 90, learningrate = 0.002,
       monitoring = (rbm, epoch) ->
             monitorexactloglikelihood!(monitor, rbm, epoch, datadict));
 
-BMPlots.plotloglikelihood(monitor, sdrange = 2.0)
+BMPlots.plotevaluation(monitor, monitorexactloglikelihood)
 
