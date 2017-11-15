e39ce9c9bb8009fbc41bceb9903d525a9a98abc4
Stefan Lenz <stefan-m-lenz@web.de>
2017-11-15 16:43:55 +0100

    spaces

--- src/BoltzmannMachines.jl
+++ src/BoltzmannMachines.jl
@@ -98,7 +98,7 @@ end
 """
     barsandstripes(nsamples, nvariables)
 Generates a test data set. To see the structure in the data set, run e. g.
-`reshape(barsandstripes(1, 16), 4,4)` a few times.
+`reshape(barsandstripes(1, 16), 4, 4)` a few times.
 
 Example from:
 MacKay, D. (2003). Information Theory, Inference, and Learning Algorithms
