a0bb7b2591b31f838571ec5fbed7cec90a185e52
Stefan Lenz <lenz@imbi.uni-freiburg.de>
2019-06-03 13:23:35 +0200

    Fixed documentation of `splitdata`

--- src/datapreprocessing.jl
+++ src/datapreprocessing.jl
@@ -180,8 +180,11 @@ end
 """
     splitdata(x, ratio)
 Splits the data set `x` randomly in two data sets `x1` and `x2`, such that
-the ratio `n2`/`n1` of the numbers of lines/samples in `x1` and `x2` is
-approximately equal to the given `ratio`.
+the fraction of samples in `x2` is equal to (or as close as possible to) the
+given `ratio`.
+
+# Example:
+    trainingdata, testdata = splitdata(data, 0.1) # Use 10 % as test data
 """
 function splitdata(x::Matrix{Float64}, ratio::Float64)
    nsamples = size(x,1)
