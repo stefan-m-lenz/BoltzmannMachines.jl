31926908b123d052e27f166b0a9c41564d72ac1e
Stefan Lenz <lenz@imbi.uni-freiburg.de>
2019-08-13 12:32:07 +0200

    Formatting of comment

--- src/gibbssampling.jl
+++ src/gibbssampling.jl
@@ -98,11 +98,14 @@ end
 
 """
     gibbssamplecond!(particles, bm, cond, nsteps)
-Conditional Gibbs sampling on the `particles` in the `bm`.
+Conditional Gibbs sampling on the `particles` in the `bm` for `nsteps`
+Gibbs sampling steps.
+
 The variables that are marked in the indexing vector `cond` are fixed
 to the initial values in `particles` during sampling. This way, conditional
 sampling is performed on these variables.
-(See also `Particles`, `initparticles`.)
+
+See also: `Particles`, `initparticles`
 """
 function gibbssamplecond!(particles::Particles, bm::AbstractBM,
       fixedvars,
